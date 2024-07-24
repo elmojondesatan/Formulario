b9b462a2b4079a5a5ef8451b413a67e1f195150a
davillatoro <davillatoro@scl.edu.gt>
2024-07-24 08:10:31 -0600

    Diseño principal

---
+++ main.js
@@ -0,0 +1,28 @@
+//Llamado al node de DOOM
+let root = document.querySelector("#root");
+
+let div_formulario = document.createElement("div");
+div_formulario.className = "div_formulario";
+
+//Lista de datos(String)
+let formulario = [
+    {nombre:"Login",link:"htpps:"},
+    {nombre:"Login",link:"htpps:"},
+    {nombre:"Login",link:"htpps:"},
+    {nombre:"Login",link:"htpps:"},
+
+];
+
+formulario.forEach(itemLista => {
+    let div_item = document.createElement("a");
+    div_item.className = "item";
+    div_item.href = itemLista.link;
+    div_item.innerText = itemLista.nombre;
+
+    div_formulario.appendChild(div_item);
+});
+
+root.appendChild(div_formulario);
+
+
+
