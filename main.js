d33080c37781f2a03e938467c1e4fdf2d239d4d8
davillatoro <davillatoro@scl.edu.gt>
2024-07-25 10:33:57 -0600

    Actualizacion general, se agregan mas carpetas

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ div_formulario.className = "div_formulario";
 //Lista de datos(String)
 let formulario = [
     {nombre:"Login",link:"componentes/login/login.html"},
-    {nombre:"Login",link:"#"},
-    {nombre:"Login",link:"#"},
-    {nombre:"Login",link:"#"},
+    {nombre:"FormulariodeBusqueda",link:"componentes/FormulariodeBusqueda/FormulariodeBusqueda.html"},
+    {nombre:"FormulariodeComentarios",link:"componentes/FormulariodeComentarios/FormulariodeComentarios.html"},
+    {nombre:"FormulariodeContacto",link:"componentes/FormulariodeContacto/FormulariodeContacto.html"},
+    {nombre:"FormulariodePedidos",link:"componentes/FormulariodePedidos/FormulariodePedidos.html"},
+    {nombre:"FormulariodeRegistro",link:"componentes/FormulariodeRegistro/ContactodeRegistro.html"},
 
 ];
 
